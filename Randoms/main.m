356b03674bf1011f8eea9c363a9cff167f013766
Dino <dinokonstantakis@dinos-macbook-pro.local>
2014-02-03 19:00:04 -0800

    Initial Commit

---
+++ Randoms/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Randoms
+//
+//  Created by Dino on 2/3/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
